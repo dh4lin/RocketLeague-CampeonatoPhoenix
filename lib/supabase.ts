4acf471020b90e4172bc2166ad0d1081df418cb4
Dhalin Carvalho <dhalinjeeh@gmail.com>
2026-02-18 03:04:52 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js"
 
 export const supabase = createClient(
   "SUA_URL_SUPABASE",
-  "SUA_ANON_KEY"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indvb2F5YmhqaW1zZGZmbWZod2xxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzODIxMjMsImV4cCI6MjA4Njk1ODEyM30.XOXRCvrKsK6Nd3kB95k5IDsklvzHP-serz8_KfpwrXQ"
 )

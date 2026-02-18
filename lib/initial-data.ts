5eea877899007deacb7c04e6ea8dde5b9636e931
Dhalin Carvalho <dhalinjeeh@gmail.com>
2026-02-18 04:07:29 -0300

    Delete lib directory

--- lib/initial-data.ts
+++
@@ -1,9 +0,0 @@
-import { supabase } from "./supabase"
-
-export async function getTeams() {
-  const { data } = await supabase
-    .from("teams")
-    .select("*")
-
-  return data || []
-}
--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js"
-
-export const supabase = createClient(
-  "https://wooaybhjimsdffmfhwlq.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indvb2F5YmhqaW1zZGZmbWZod2xxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzODIxMjMsImV4cCI6MjA4Njk1ODEyM30.XOXRCvrKsK6Nd3kB95k5IDsklvzHP-serz8_KfpwrXQ"
-)
